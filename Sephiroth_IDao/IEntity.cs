0ab2e17a5f3f26b9ac3a217ac3f58dd511f7950e
sephirothwzc <zhanchao.wu@icloud.com>
2015-09-22 10:31:29 +0800

    2015年9月22日10:31:19 吴占�

---
+++ Sephiroth_IDao/IEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*************************************************************************************
+  * CLR 版本：       4.0.30319.33440
+  * 类 名 称：       IEntity
+  * 机器名称：       SEPHIROTHBF0C
+  * 命名空间：       Sephiroth_IDao
+  * 文 件 名：       IEntity
+  * 创建时间：       2015/09/22 10:30:53
+  * 作    者：       吴占超
+  * 说    明：
+  * 修改时间：
+  * 修 改 人：
+ *************************************************************************************/
+
+namespace Sephiroth_IDao
+{
+    public class IEntity
+    {
+    }
+}
